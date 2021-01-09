2090837ac28459fd949ed21a2fef294fb2cf29fb
twinstae <rabolution@gmail.com>
2021-01-09 18:03:45 +0900

    delete webVitals

--- example-app/src/redux/store.js
+++ example-app/src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 
 const defaultState = {checked: false};
+
 const reducer = function(state = defaultState, action){
     switch (action.type){
         case 'TOGGLE':
