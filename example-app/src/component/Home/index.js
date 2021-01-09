22044e5d19f565ee31e562b971e95dd2cd3097db
twinstae <rabolution@gmail.com>
2021-01-09 18:59:49 +0900

    Home componentWillMount

--- example-app/src/component/Home/index.js
+++ example-app/src/component/Home/index.js
@@ -1,13 +1,26 @@
-import Banner from './Banner';
-import MainView from './MainView.js';
 import React from 'react';
 import {connect} from 'react-redux';
+
+import Banner from './Banner';
+import MainView from './MainView.js';
+
+import agent from '../../agents';
+
+const Promise = global.Promise;
 
 const mapStateToProps = state => ({
     appName: state.appName
 });
 
+const mapDispachToProps = dispatch => ({
+    onLoad: (payload) =>
+        dispatch({type:'HOME_PAGE_LOADED', payload}),
+})
+
 class Home extends React.Component{
+    componentWillMount() {
+        this.props.onLoad(agent.Articles.all());
+    }
     render(){
         return (
             <div className="home-page">
@@ -28,4 +41,4 @@ class Home extends React.Component{
     }
 }
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, mapDispachToProps)(Home);
