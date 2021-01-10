b205cacfdec1c78e7aa503051541cd6732ad15d8
twinstae <rabolution@gmail.com>
2021-01-10 13:41:21 +0900

    add auth reducer, actionType ASYNC_START

--- example-app/src/constants/actionTypes.js
+++ example-app/src/constants/actionTypes.js
@@ -5,3 +5,4 @@ export const LOGIN = 'LOGIN';
 export const LOGIN_PAGE_UNLOADED = 'LOGIN_PAGE_UNLOADED';
 export const REGISTER = 'REGISTER';
 export const REGISTER_PAGE_UNLOADED = 'REGISTER_PAGE_UNLOADED';
+export const ASYNC_START = 'ASYNC_START';
--- example-app/src/redux/reducer.js
+++ example-app/src/redux/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import { routerReducer } from 'react-router-redux';
 import article from './reducers/article';
 import articleList from './reducers/articleList';
+import auth from './reducers/auth';
 import common from './reducers/common';
 
 export default combineReducers({
     articleList,
     article,
+    auth,
     common,
     router: routerReducer
 })
